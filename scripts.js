42583b4750da9b56c79bf56636fde5e85264131a
andrew lupton <aflupton@gmail.com>
2018-07-10 15:28:54 -0700

    add combined reducer, and refactor

--- scripts.js
+++ scripts.js
@@ -72,6 +72,16 @@ const songChangeReducer = (state = initialState.currentSongId, action) => {
   }
 }
 
+const rootReducer = this.Redux.combineReducers({
+  currentSongId: songChangeReducer,
+  songsById: lyricChangeReducer
+});
+
+// REDUX STORE
+// creates store and passes a reducer as an argument
+const { createStore } = Redux;
+const store = createStore(rootReducer);
+
 // JEST TESTS + SETUP
 const { expect } = window;
 
@@ -115,63 +125,96 @@ expect(songChangeReducer(initialState, { type: null })).toEqual(initialState);
 
 expect(songChangeReducer(initialState, { type: 'CHANGE_SONG', newSelectedSongId: 1 })).toEqual(1);
 
-// REDUX STORE
-// creates store and passes a reducer as an argument
-const { createStore } = Redux;
-const store = createStore(lyricChangeReducer);
+expect(rootReducer(initialState, { type: null })).toEqual(initialState);
+expect(store.getState().currentSongId).toEqual(songChangeReducer(undefined, { type: null }));
+expect(store.getState().songsById).toEqual(lyricChangeReducer(undefined, { type: null }));
 
 // RENDERING STATE IN DOM
 const renderSongs = () => {
+  // Retrieves songsById state slice from store:
   const songsById = store.getState().songsById;
+  // Cycles through each key in songsById:
   for (const songKey in songsById) {
+    // Locates song corresponding with each key, saves as 'song' constant:
     const song = songsById[songKey]
+    // Creates <li>, <h3>, and <em> HTMl elements to render this song's information in the DOM:
     const li = document.createElement('li');
     const h3 = document.createElement('h3');
     const em = document.createElement('em');
+    // Creates text node containing each song's title:
     const songTitle = document.createTextNode(song.title);
+    // Creates text node containing each song's artist:
     const songArtist = document.createTextNode(' by ' + song.artist);
+    // Adds songTitle text node to the <em> element we created 3 lines up:
     em.appendChild(songTitle);
+    // Adds <em> element that now contains song title to <h3> element created 5 lines up:
     h3.appendChild(em);
+    // Also adds songArtist text node created 2 lines up to <h3> element  created 6 lines up:
     h3.appendChild(songArtist);
+    // Adds click event listener to same  <h3> element, when this <h3> is clicked, an event handler called selectSong() will run, using song's ID as argument:
     h3.addEventListener('click', function() {
       selectSong(song.songId);
     });
+    // Adds entire <h3> element to the <li> element created 11 lines above:
     li.appendChild(h3);
+    // Appends this <li> element to the <ul> in index.html with a 'songs' ID:
     document.getElementById('songs').appendChild(li);
   }
 }
 
-// const renderLyrics = () => {
-//   // Defines a lyricsDisplay constant referring to the div with a 'lyrics' ID in index.html
-//   const lyricsDisplay = document.getElementById('lyrics');
-//   // If there are already lyrics in this div, remove them one-by-one until it is empty:
-//   while (lyricsDisplay.firstChild) {
-//     lyricsDisplay.removeChild(lyricsDisplay.firstChild);
-//   }
-//   // Locates the song lyric at the current arrayPosition:
-//   const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
-//   // Creates DOM text node containing the song lyric identified in line above:
-//   const renderedLine = document.createTextNode(currentLine);
-//   // Adds text node created in line above to 'lyrics' div in DOM
-//   document.getElementById('lyrics').appendChild(renderedLine);
-// }
-// // Runs renderLyrics() method from above when page is finished loading.
-// // window.onload is HTML5 version of jQuery's $(document).ready()
-// window.onload = function() {
-//   renderLyrics();
-// }
-//
-// // CLICK LISTENER
-// const userClick = () => {
-//   // Runs 'restart_song' action after cycling thru array
-//   if (store.getState().arrayPosition === store.getState().songLyricsArray.length - 1) {
-//     store.dispatch({ type: 'RESTART_SONG' });
-//   } else {
-//     // Otherwise, continues cycling thru array positions
-//     store.dispatch({ type: 'NEXT_LYRIC' });
-//   }
-// }
-//
-// // SUBSCRIBE TO REDUX STORE
-// // This callback is required to receive updated state values from the store, and thus render to the DOM
-// store.subscribe(renderLyrics);
+const renderLyrics = () => {
+  // Defines a lyricsDisplay constant referring to the div with a 'lyrics' ID in index.html
+  const lyricsDisplay = document.getElementById('lyrics');
+  // If there are already lyrics in this div, remove them one-by-one until it is empty:
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  // Locates the song lyric at the current arrayPosition:
+  if (store.getState().currentSongId) {
+    // Creates DOM text node containing the song lyric identified in line above:
+    const currentLine = document.createTextNode(store.getState().songsById[store.getState().currentSongId].songArray[store.getState().songsById[store.getState().currentSongId].arrayPosition]);
+    // Adds text node created in line above to 'lyrics' div in DOM
+    document.getElementById('lyrics').appendChild(currentLine);
+  } else {
+    const selectSongMessage = document.createTextNode("Select a song from the menu above to sing along!");
+    document.getElementById('lyrics').appendChild(selectSongMessage);
+  }
+}
+
+// Runs renderLyrics() method from above when page is finished loading.
+// window.onload is HTML5 version of jQuery's $(document).ready()
+window.onload = function() {
+  renderSongs();
+  renderLyrics();
+}
+
+// CLICK LISTENER
+const userClick = () => {
+  // Runs 'restart_song' action after cycling thru array
+  if (store.getState().songsById[store.getState().currentSongId].arrayPosition === store.getState().songsById[store.getState().currentSongId].length - 1) {
+    store.dispatch({ type: 'RESTART_SONG', currentSongId: store.getState().currentSongId });
+  } else {
+    // Otherwise, continues cycling thru array positions
+    store.dispatch({ type: 'NEXT_LYRIC', currentSongId: store.getState().currentSongId });
+  }
+}
+
+const selectSong = (newSongId) => {
+  let action;
+  if (store.getState().currentSongId) {
+    action = {
+      type: 'RESTART_SONG',
+      currentSongId: store.getState().currentSongId
+    }
+    store.dispatch(action);
+  }
+  action = {
+    type: 'CHANGE_SONG',
+    newSelectedSongId: newSongId
+  }
+  store.dispatch(action);
+}
+
+// SUBSCRIBE TO REDUX STORE
+// This callback is required to receive updated state values from the store, and thus render to the DOM
+store.subscribe(renderLyrics);
